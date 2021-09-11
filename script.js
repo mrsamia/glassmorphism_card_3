3a5d28e15a7426755df63ca206e064a51f5962a8
mrsamia <15203060@iubat.edu>
2021-09-11 07:56:37 -0700

    SRM | add_js_function

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-document.getElementById("message").addEventListener("click", di);
 
-function di() {
-  document.getElementById("message").style.color='White';
-  document.getElementById("message").innerHTML ="Sent";
-  document.getElementById("message").style.backgroundColor = "#8D2D35";
+function myFunction() {
+  var img = document.getElementById("id1");
+  if(img.innerHTML=="Message"){
+  img.innerHTML = "Sent";
+  img.style.color='white';
+  img.style.backgroundColor = "#FF7A7A";
   
-}
+ }
+ else if(img.innerHTML=="Sent"){
+ img.innerHTML = "Message";
+ img.style.color='White';
+ img.style.backgroundColor = "#5C5859";
 
-document.getElementById("follow").addEventListener("click",dis);
-function dis(){
-    document.getElementById("follow").color="white";
-    document.getElementById("follow").innerHTML="unfollow";
-    document.getElementById("follow").style.backgroundColor = "#D3A06A";
-}
+ }
+ 
+ }
+
+
+ function myFunction_2(){
+   var img_2 = document.getElementById("id2");
+   if(img_2.innerHTML=="Follow"){
+     img_2.innerHTML="Unfollow";
+     img_2.style.color="white";
+     img_2.style.backgroundColor="#D3A06A";
+   }
+   else if(img_2.innerHTML=="Unfollow"){
+    img_2.innerHTML="Follow";
+    img_2.style.color="white";
+    img_2.style.backgroundColor="#5C5859";
+   }
+ }
